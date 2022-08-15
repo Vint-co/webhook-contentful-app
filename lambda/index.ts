e91e40c12c4de9bd2d09eb4ea18d35732637b309
PatrickTSanders <patricktsanders@gmail.com>
2022-08-15 11:03:57 -0400

    webpack + ts loader

---
+++ lambda/index.ts
@@ -0,0 +1,31 @@
+import CodePipeline from 'aws-sdk/clients/codepipeline';
+
+export const lambdaHandler = (
+  event: CustomEvent
+  //   context: Context,
+  //   callback: void
+): void => {
+  // Retrieve the CodePipeline name
+  const pipelineName = event.pipelineName;
+
+  //Instantiate CodePipeline
+  const codepipeline = new CodePipeline();
+
+  const params = {
+    name: pipelineName,
+  };
+
+  codepipeline.startPipelineExecution(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+    } else {
+      console.log(data); // successful response
+    }
+  });
+
+  return;
+};
+
+type CustomEvent = {
+  pipelineName: string;
+};
